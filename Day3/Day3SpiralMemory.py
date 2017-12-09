29b1f2422873f7e3a3592e025a5896973246e365
Quinn Ramsay <qramsay@sfu.ca>
2017-12-08 23:51:16 -0700

    Finished part 2, slightly unorthodox but levies the previous fncs

--- Day3/Day3SpiralMemory.py
+++ Day3/Day3SpiralMemory.py
@@ -5,6 +5,7 @@ Created on Thu Dec  7 12:56:51 2017
 @author: WFN53616
 """
 import math;
+import numpy as np;
 
 def CalcBottomRightRingNum(ringNum):
     return 1 + 4*(ringNum*(ringNum + 1));
@@ -65,9 +66,64 @@ def GetManhattanDistance(coordinates):
     
 def GetShortestPath(num):
     return GetManhattanDistance(GetSpiralCoordinates(num));
+    
+def SumAdjacents(coords, grid):
+    sum = 0;
+    for i in [-1,0,1]:
+        for j in [-1,0,1]:
+            nextCoordX = coords[0] + i;
+            nextCoordY = coords[1] + j;
+            
+            if((i == 0 and j == 0) or (abs(nextCoordX) > (len(grid) + 1)/2) or (abs(nextCoordY) > (len(grid[nextCoordX])+1)/2)):
+                continue;
+            sum += grid[nextCoordX][nextCoordY];
+    return sum;
+    
+    
+def GetFirstNumberOfSumSpiralGreaterThanNum(num):
+    #spiral ring of this sum spiral is at least smaller than
+    #the single counting version
+    #except for first ring numbers
+    #not accurate for numbers below 10
+    #this is due to the adjacencies wrapping because my holding data structure is based from
+     #top left instead of center = 0,0
+    
+    countingRingNum = GetRingNum(num);
+    traverseFinish = num;
+    if(num < 10):
+        traverseFinish = 10;
+        countingRingNum = GetRingNum(traverseFinish);
+        
+    dim = 1 + countingRingNum * 2;
+    #start with grid of zeros
+    
+    grid = np.zeros((dim, dim));
+    #[0][0] is 1. -coordinates should wrap
+    grid[0][0] = 1;
 
+    for i in range(1, traverseFinish):
+        # + 1 to align with original spiral
+        coords = GetSpiralCoordinates(i + 1);
+        sum = SumAdjacents(coords, grid);
+        if(sum > num):
+            return sum;
+        grid[coords[0]][coords[1]] = sum;
+    
+    return num;
+    
+    
 
-"""print (GetDifferenceFromBottomRightRingNum( 0, 1) == 0);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(1) == 2);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(5) == 10);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(6)== 10);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(8)== 10);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(9)== 10);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(10)== 11);
+"""print(GetFirstNumberOfSumSpiralGreaterThanNum(11) == 23);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(142) == 147);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(655) == 747);
+print(GetFirstNumberOfSumSpiralGreaterThanNum(200) == 304);
+print (GetDifferenceFromBottomRightRingNum( 0, 1) == 0);
 print (GetDifferenceFromBottomRightRingNum(1, 9) == 0);
 print (GetDifferenceFromBottomRightRingNum(2, 25) == 0);
 print (GetDifferenceFromBottomRightRingNum(3, 49) == 0);
@@ -106,4 +162,5 @@ print(GetShortestPath(12)==3);
 print(GetShortestPath(23)==2);
 print(GetShortestPath(1024)==31);"""
     
-print(GetShortestPath(368078));
+#print(GetShortestPath(368078));
+#print(GetFirstNumberOfSumSpiralGreaterThanNum(368078));
