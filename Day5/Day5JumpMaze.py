1ce80218ff65e3330f59e2358965847b83017ed7
Quinn Ramsay <qramsay@sfu.ca>
2017-12-09 14:11:17 -0700

    Day5 Part 2

--- Day5/Day5JumpMaze.py
+++ Day5/Day5JumpMaze.py
@@ -1,21 +1,29 @@
-def CalculateJumpStepsFromList(input):
+def GetNewJumpNum(prevValue):
+    if(prevValue >= 3):
+        return prevValue - 1;
+    return prevValue + 1;
+    
+def CalculateJumpStepsFromList(input, useModJumps):
     currentIndex = 0;
     steps = 0;
     while(len(input) > currentIndex >= 0 ):
         nextIndex = currentIndex + input[currentIndex];
-        input[currentIndex] += 1;
+        if(useModJumps):
+            input[currentIndex] = GetNewJumpNum(input[currentIndex]);
+        else:
+            input[currentIndex] += 1;
         steps+=1;
         currentIndex = nextIndex;
     return steps;
     
-def CalculateJumpStepsFromFilename(filename):
+def CalculateJumpStepsFromFilename(filename, useModJumps):
     inputfile = open(filename);
     input = [];
     for line in inputfile.readlines():
         num = int(line.strip());
         input.append(num);
-    return CalculateJumpStepsFromList(input);
+    return CalculateJumpStepsFromList(input, useModJumps);
     
 
-#print(CalculateJumpStepsFromFilename("testInput.txt"));
-print(CalculateJumpStepsFromFilename("input.txt"));
+#print(CalculateJumpStepsFromFilename("testInput.txt", True));
+print(CalculateJumpStepsFromFilename("input.txt", True));
