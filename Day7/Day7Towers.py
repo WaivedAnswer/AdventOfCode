e60d1a448acc46a0a23ccc5c961d5cd730d5972a
Quinn Ramsay <qramsay@sfu.ca>
2017-12-11 12:22:36 -0700

    Finished Day7

--- Day7/Day7Towers.py
+++ Day7/Day7Towers.py
@@ -1,90 +1,65 @@
 #day7
 class Tower:
-    def __init__(self, name, weight, subTowers):
+    def __init__(self, name, weight, aboves):
         self.name = name;
         self.weight = weight;
-        self.subTowers = subTowers;
+        self.aboves = aboves;
         
     
 def ParseInformation(information):
     words = information.split();
     name = words[0];
-    weight = words[1].strip("(").strip(")");
+    weight = int(words[1].strip("(").strip(")"));
     aboves = [];
     if("->" in words):
         aboveString = "".join(words[3:]);
         aboves = aboveString.strip().split(",");
     return (name, weight, aboves);
     
-def GetCurrentLevelKey(keyLookups, currentLevelKeys):
-    for key in keyLookups:
-        if(key in currentLevelKeys):
-            return key;
-    print key, keyLookups, currentLevelKeys
-    print("Something went wrong.");
-    return None;
+def GetTowerWeights(towers, currKey):
     
-def GetNodeParent(name, towersKeyLookups, towers):
-    nextKey = GetCurrentLevelKey(towersKeyLookups[name], towers.keys());
-    currentTower = towers[nextKey];
-    oldTower = None;
-    #traverse tree
-    while(True):
-        nextKey = GetCurrentLevelKey(towersKeyLookups[name], currentTower.subTowers.keys());
-        nextTower = currentTower.subTowers[nextKey];
-        if(nextTower.name == name):
-            break;
-        currentTower = nextTower;
+    currTower = towers[currKey];
+    weight = currTower.weight;
+    if(len(currTower.aboves) == 0):
+        return weight;
+        
+    weights = [];
+    keys = [];
+    setWeights = [];
+    differentWeight = (None, None);
     
-    return currentTower;
-    
-def AppendLookupNameToAllChildren(tower, towersKeyLookups, name):
-    for subtower in tower.subTowers:
-        print subtower;
-        AppendLookupNameToAllChildren(subtower, towersKeyLookups, name);
-        if(name not in towersKeyLookups[subTower.name]):
-            towersKeyLookups[subTower.name].append(name);
+    for above in currTower.aboves:
+        subWeight = GetTowerWeights(towers, above);
+        keys.append(above);
+        weights.append(subWeight);
+        setWeights = set(weights);
+        
+    if(len(setWeights) > 1): 
+        print setWeights, weights
+        requiredWeight = [w for w in setWeights if weights.count(w) > 1][0];
+        diffWeight = [w for w in setWeights if weights.count(w) == 1][0];
+        baseWeightIndex = weights.index(diffWeight);
+        baseWeight = towers[keys[baseWeightIndex]].weight;
+        print( "Different weight needs to be: " + str(requiredWeight));
+        print ("Different weight needs to change to: " + str(baseWeight - (diffWeight - requiredWeight)) );
+        
+    weight += sum(weights);
+    return weight;
     
 def GetBottomTower(input):
     towers = {};
-    towersKeyLookups = {};
-    
+    subTowers = {};
     for information in input:
         (name, weight, aboves) = ParseInformation(information);
-        #create subtowers
-        subTowers = {};
-        for aboveName in aboves:
-            # has to be on root
-            if(aboveName in towers.keys()):
-                aboveTower = towers.pop(aboveName, None);
-                if(aboveTower != None): 
-                    subTowers[aboveName] = aboveTower;
-                else:
-                    print ("Error");
-            else:
-                subTowers[aboveName] = Tower(aboveName, 0, {})
-            if(aboveName not in towersKeyLookups):
-                towersKeyLookups[aboveName] = [];
-            print subTowers[aboveName];
-            #AppendLookupNameToAllChildren(subTowers[aboveName], towersKeyLookups, tower.name);
-        
-                
-        tower = Tower(name, weight, subTowers);
-
-                
-        #add to root      
-        if(name not in towersKeyLookups):
-            towers[tower.name] = tower;
-            towersKeyLookups[tower.name] = [""];
-        #replace default on parent
-        else:
-             parent = GetNodeParent(tower.name, towersKeyLookups, towers);
-             print parent
-             for key in towersKeyLookups[parent.name]:
-                 AppendLookupNameToAllChildren(tower, towersKeyLookups, key);
-             parent.subTowers[tower.name] = tower;
-             
-    print ("BottomTower is: " + towers.values()[0].name);
+        tower = Tower(name,weight,aboves);
+        for name in aboves:
+            subTowers[name] = True;
+        towers[tower.name] = tower;
+    bottom = [key for key in towers.keys() if key not in subTowers.keys()];
+    weight = GetTowerWeights(towers, bottom[0]);
+    
+    print bottom;
+    print ("Total weight: " + str(weight));
     
 def GetBottomTowerFromFileInput(filename):
     inputFile = open(filename);
