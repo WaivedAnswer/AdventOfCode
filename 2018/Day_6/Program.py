01ddaaf3720f4b753e73d753ff6547357933959c
Quinn Ramsay <quinnramsay@gmail.com>
2018-12-06 17:53:26 -0700

    not sure if im moving forward at this point or backwards

--- 2018/Day_6/Program.py
+++ 2018/Day_6/Program.py
@@ -5,6 +5,10 @@ from collections import defaultdict
 from collections import OrderedDict
 import itertools
 
+
+def distFromOrigin(x,y):
+	return manhattanDist(x,y,0,0)
+	
 def manhattanDist(x1,y1,x2,y2):
     return abs(x1-x2) + abs(y1-y2)
     
@@ -19,37 +23,76 @@ def getClosest(coords, coord):
             closest = (x,y)
             minD = dist
     return closest
+	
+def getDistVectors(dist):
+	possibles = range(-dist, dist + 1)
+	return [ pair for pair in itertools.permutations(possibles, 2) if distFromOrigin(pair[0], pair[1]) == dist]
+	
     
-def findClosest(coords, coord, minX, minY, maxX, maxY, seen):
-    closest = getClosest(coords, coord)
-    if(closest and (closest[0] == minX or closest[1] == minY or closest[0] == maxX or closest[1] == maxY)):
-        return None
-    return closest
+def getCoordsAtDist(coord, dist):
+	if(dist == 0):
+		return [coord]
+	dists = getDistVectors(dist)
+	preSet = [(coord[0] + dist[0], coord[1] + dist[1]) for dist in dists]
+	return preSet
     
-def growFromCoord(coordMap, closest, coord):
-    
-def countClosest(coords):
-    closest = {}
-    coordMap = defaultdict(lambda: defaultdict(int))
-    minX = min(coords, key = lambda x:x[0])
-    minY = min(coords, key = lambda x:x[1])
-    maxX = max(coords, key = lambda x:x[0])
-    maxY = max(coords, key = lambda x:x[1])
-    edgeCoords = set([coord for coord in coords if coord[0] == minX or coord[0] == maxX or coord[1] == minY or coord[1] == maxY])
-    for coord in coords:
-        
+def countClosest(coords, names):
+	closest = defaultdict(tuple)
+	minX = min(coords, key = lambda x:x[0])[0]
+	minY = min(coords, key = lambda x:x[1])[1]
+	maxX = max(coords, key = lambda x:x[0])[0]
+	maxY = max(coords, key = lambda x:x[1])[1]
+	edgeCoords = set([coord for coord in coords if coord[0] == minX or coord[0] == maxX or coord[1] == minY or coord[1] == maxY])
+	#print(minX, minY, maxX, maxY, edgeCoords)
+	stillGrowing = True
+	growDist = 0
+	coordTotals = defaultdict(int)
+	while stillGrowing:
+		stillGrowing = False
+		newCoords = set()
+		coordTouches = defaultdict(set)
+		for coord in coords:
+			touches = getCoordsAtDist(coord, growDist)
+			coordTouches[coord] = touches
+			newCoords = newCoords.union({tup for tup in coordTouches[coord] if tup not in newCoords})
+		for coord in coordTouches:
+			for touch in coordTouches[coord]:
+				if(touch in closest or touch[0] < 0 or touch[1] < 0): # already has closest
+					continue
+				elif(touch not in newCoords): #tie
+					closest[touch] = None
+				else:
+					closest[touch] = coord
+				if(coord not in edgeCoords):
+					stillGrowing = True
+		growDist += 1
+		
+	for i in range(0, maxX + 1):
+		for j in range(0, maxY + 2):
+			if(closest[(i,j)] == None or closest[i,j] == ()):
+				print(".", end = "")
+			elif((i,j) not in coords):
+				print(names[closest[(i,j)]].lower(), end = "")
+			else:
+				print(names[closest[(i,j)]], end = "")
+		print("")
+	
+	#print (coordTotals)
+	return Common.maxValuePair(coordTotals)
     
     
 def part1(input):
-    coords = []
-    coordNum = 1
-    for line in input:
-        numbers = Common.numbers(line)
-        x = numbers[0]
-        y = numbers[1]
-        coords.append((x,y))
-    
-    return countClosest(coords)
+	coords = []
+	names = {}
+	coordName = 'A'
+	for line in input:
+		numbers = Common.numbers(line)
+		y = numbers[0]
+		x = numbers[1]
+		coords.append((x,y))
+		names[(x,y)] = coordName
+		coordName = chr(ord(coordName) + 1)
+	return countClosest(coords, names)
             
 input = Common.inputAsLines()
 #input = Common.inputAsString()
