e7d685627633d0d01fb123bf4a8cfc0a14286685
Quinn Ramsay <quinnramsay@gmail.com>
2018-12-07 11:23:52 -0700

    rewrite day 6, it's done

--- 2018/Day_6/Program.py
+++ 2018/Day_6/Program.py
@@ -1,133 +1,89 @@
 #!/usr/bin/env python#
 import Common
-import re
 from collections import defaultdict
-from collections import OrderedDict
-import itertools
 
 
 def distFromOrigin(x, y):
-	return manhattanDist(x, y, 0, 0)
+    return manhattanDist(x, y, 0, 0)
 
 
 def manhattanDist(x1, y1, x2, y2):
-       return abs(x1 - x2) + abs(y1 - y2)
+    return abs(x1 - x2) + abs(y1 - y2)
 
-
-def getClosest(coords, coord):
-       minD = 99999999
-       closest = None
-       for x, y in coords:
-              dist = manhattanDist(x, y, coord[0], coord[1])
-              if(dist == minD):
-                     closest = None
-              elif(dist < minD):
-                     closest = (x, y)
-                     minD = dist
-       return closest
-
-
-def getDistVectors(dist):
-	possibles = range(-dist, dist + 1)
-	return [pair for pair in itertools.product(possibles, repeat=2) if distFromOrigin(pair[0], pair[1]) == dist]
-
-
-def getCoordsAtDist(coord, dist):
-	if(dist == 0):
-		return [coord]
-	dists = getDistVectors(dist)
-	preSet = [(coord[0] + dist[0], coord[1] + dist[1]) for dist in dists]
-	return preSet
-
-
-def printClosest(closest, maxX, maxY, names, coords):
-       mapC = "\n"
-       for i in range(0, maxX):
-              for j in range(0, maxY):
-                     if((i, j) not in closest):
-                            mapC += "0"
-                     elif(closest[(i, j)] == None or closest[i, j] == ()):
-                            mapC += "."
-                     elif((i, j) not in coords):
-                            mapC += names[closest[(i, j)]].lower()
-                     else:
-                            mapC += names[closest[(i, j)]]
-              mapC += "\n"
-       print(mapC)
-       
-def getTotals(closest, maxX, maxY, names, coords):
-       coordTotals = defaultdict(int)
-       for i in range(0, maxX):
-              for j in range(0, maxY):
-                     if((i, j) not in closest):
-                            continue
-                     elif(closest[(i, j)] == None or closest[i, j] == ()):
-                            continue
-                     elif((i, j) not in coords):
-                            coordTotals[names[closest[(i, j)]]] += 1
-                     else:
-                            coordTotals[names[closest[(i, j)]]] += 1
-       return coordTotals
 
 def countClosest(coords, names):
-       closest = defaultdict(tuple)
-       minX = min(coords, key=lambda x: x[0])[0]
-       minY = min(coords, key=lambda x: x[1])[1]
-       maxX = max(coords, key=lambda x: x[0])[0]
-       maxY = max(coords, key=lambda x: x[1])[1]
-       edgeCoords = set([coord for coord in coords if coord[0] == minX or coord[0] == maxX or coord[1] == minY or coord[1] == maxY])
-       stillGrowing = True
-       growDist = 0
-       growingCoords = coords
-       while stillGrowing:
-              #printClosest(closest, maxX, maxY, names, coords)
-              stillGrowing = False
-              newCoords = set()
-              coordTouches = defaultdict(set)
-              ties = set()
-              toRemove = []
-              for coord in growingCoords:
-                     touches = getCoordsAtDist(coord, growDist)
-                     coordTouches[coord] = [touch for touch in touches if not touch in closest or touch[0] < 0 or touch[1] < 0]
-                     if(not coordTouches):
-                            toRemove.append(coord)
-                     ties = ties.union(newCoords.intersection({tup for tup in touches}))
-                     newCoords = newCoords.union({tup for tup in touches})
-                     
-              growingCoords = [coord in growinCoords if coord not in toRemove]
-              
-              for coord in coordTouches:
-                     for touch in coordTouches[coord]:
-                            if(touch in ties): # tie 
-                                   closest[touch] = None 
-                            else: 
-                                   closest[touch] = coord 
-                            if coord not in edgeCoords: 
-                                   stillGrowing = True
+    closest = defaultdict(lambda: [])
+    minX = min(coords, key=lambda x: x[0])[0]
+    minY = min(coords, key=lambda x: x[1])[1]
+    maxX = max(coords, key=lambda x: x[0])[0]
+    maxY = max(coords, key=lambda x: x[1])[1]
+    coordTotals = defaultdict(int)
+    infiniteAreas = set()
+    for i in range(minX - 1, maxX + 1):
+        for j in range(minY - 1, maxY + 1):
+            dists = {p: manhattanDist(p[0], p[1], i, j) for p in coords}
+            close = min(dists.items(), key=lambda p: p[1])
+            minDist = close[1]
+            if list(dists.values()).count(minDist) > 1:
+                continue
+            if (i == minX or i == maxX or j == minY or j == maxY) and list(
+                dists.values()
+            ).count(minDist) == 1:
+                infiniteAreas.update(
+                    {names[k] for k, d in dists.items() if d == minDist}
+                )
 
-              growDist += 1
-              print(growDist)
-       #printClosest(closest, maxX, maxY, names, coords)
-       totals = getTotals(closest, maxX, maxY, names, coords)
-       return Common.maxValuePair(totals)
-    
-    
+            coordTotals[close[0]] += 1
+    coordTotals = {
+        key: total
+        for (key, total) in coordTotals.items()
+        if names[key] not in infiniteAreas
+    }
+    return Common.maxValuePair(coordTotals)
+
+
+def regionCloseToAll(coords, minDist):
+    closest = defaultdict(lambda: [])
+    minX = min(coords, key=lambda x: x[0])[0]
+    minY = min(coords, key=lambda x: x[1])[1]
+    maxX = max(coords, key=lambda x: x[0])[0]
+    maxY = max(coords, key=lambda x: x[1])[1]
+
+    regionSum = 0
+    for i in range(minX - 1, maxX + 1):
+        for j in range(minY - 1, maxY + 1):
+            dists = {p: manhattanDist(p[0], p[1], i, j) for p in coords}
+            totalDist = sum([total for (key, total) in dists.items()])
+            if totalDist < minDist:
+                regionSum += 1
+    return regionSum
+
+
 def part1(input):
-	coords = []
-	names = {}
-	coordName = 'A'
-	for line in input:
-		numbers = Common.numbers(line)
-		y = numbers[0]
-		x = numbers[1]
-		coords.append((x,y))
-		names[(x,y)] = coordName
-		coordName = chr(ord(coordName) + 1)
-	return countClosest(coords, names)
-            
+    names, coords = parseCoords(input)
+    return countClosest(coords, names)
+
+
+def parseCoords(input):
+    coords = []
+    names = {}
+    coordName = "A"
+    for line in input:
+        numbers = Common.numbers(line)
+        x = numbers[0]
+        y = numbers[1]
+        coords.append((x, y))
+        names[(x, y)] = coordName
+        coordName = chr(ord(coordName) + 1)
+    return names, coords
+
+
+def part2(input):
+    names, coords = parseCoords(input)
+    return regionCloseToAll(coords, 10000)
+
+
 input = Common.inputAsLines()
-# input = Common.inputAsString()
-# print(getDistVectors(2))
-# assert(manhattanDist())
+
 print(part1(input))
-# print(part2(input))
+print(part2(input))
