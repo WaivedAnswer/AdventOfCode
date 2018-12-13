a4c3c7a1620e0f0f37c8f1e0753327fb740820ae
Quinn Ramsay <quinnramsay@gmail.com>
2018-12-13 14:39:26 -0700

    Completed based on the repeating pattern

--- 2018/Day_12/Program.py
+++ 2018/Day_12/Program.py
@@ -22,72 +22,65 @@ def assignNewPlant(newGen, oldGen, patterns, index):
 
 def printPlantArray(plantArray, gen):
        print("Gen:" + str(gen))
-       for i in range(-3,35):
+       for i in range(0,max(plantArray)):
               print(getPlantAtIndex(plantArray, i), end = "")
        print("")
+	   
+def sumTotalPlants(plantArray):
+	   return sum(key for key,value in plantArray.items() if value =="#")
+	   
 def part1(input):
-       isInitial = True
-       plantArray = defaultdict(str)
-       patterns = defaultdict(str)
-       for line in input:
-              if(isInitial):
-                     initialPlants = re.sub("[^#.]", "", line)
-                     plantArray = {item[0]:item[1] for item in zip(range(len(initialPlants)), initialPlants) }
-                     isInitial = False
-              else:
-                     patternPair = line.strip().split('=>')
-                     pattern = patternPair[0].strip()
-                     result = patternPair[1].strip()
-                     patterns["".join(pattern)] = result
-       #printPlantArray(plantArray, 0)
-       minPlant = 0
-       maxPlant = len(plantArray) - 1
-       initialPlantArray = {}
-       initialPlantArray.update(plantArray)
-       
-       cycle = None
-       
-       for i in range(1, 50000000001):
-              print(i)
-              nextGen = defaultdict(str)
-              
-              assignNewPlant(nextGen, plantArray, patterns, minPlant - 1)
-              assignNewPlant(nextGen, plantArray, patterns, minPlant - 2)
-              assignNewPlant(nextGen, plantArray, patterns, maxPlant + 1)
-              assignNewPlant(nextGen, plantArray, patterns, maxPlant + 2)
-       
-              for j in plantArray:
-                     assignNewPlant(nextGen, plantArray, patterns, j)
-                     
-              plantArray = nextGen
-              minPlant = minPlant - 2
-              maxPlant = maxPlant + 2
+	isInitial = True
+	plantArray = defaultdict(str)
+	patterns = defaultdict(str)
+	for line in input:
+		if(isInitial):
+			initialPlants = re.sub("[^#.]", "", line)
+			plantArray = {item[0]:item[1] for item in zip(range(len(initialPlants)), initialPlants) }
+			isInitial = False
+		else:
+			patternPair = line.strip().split('=>')
+			pattern = patternPair[0].strip()
+			result = patternPair[1].strip()
+			patterns["".join(pattern)] = result
+	printPlantArray(plantArray, 0)
+	minPlant = 0
+	maxPlant = len(plantArray) - 1
+	initialPlantArray = {}
+	initialPlantArray.update(plantArray)
+	
+	cycle = None
+	totalPlants = sumTotalPlants(plantArray)
+	lastTotalDiff = None
+	repeatingGen = None
+	totalAtRepeat = None
+	for i in range(1, 5000):
+		nextGen = defaultdict(str)
+		
+		assignNewPlant(nextGen, plantArray, patterns, minPlant - 1)
+		assignNewPlant(nextGen, plantArray, patterns, minPlant - 2)
+		assignNewPlant(nextGen, plantArray, patterns, maxPlant + 1)
+		assignNewPlant(nextGen, plantArray, patterns, maxPlant + 2)
+		
+		for j in plantArray:
+			assignNewPlant(nextGen, plantArray, patterns, j)
+			
+		plantArray = nextGen
+		minPlant = minPlant - 2
+		maxPlant = maxPlant + 2
 
-              if(set(initialPlantArray.items()).issubset(set(plantArray.items()))):
-                     cycle = i
-                     break
-                     
-       print(cycle)
-       plantArray = initialPlantArray
-       for i in range(1, 50000000001 % cycle):
-              print(i)
-              nextGen = defaultdict(str)
-              
-              assignNewPlant(nextGen, plantArray, patterns, minPlant - 1)
-              assignNewPlant(nextGen, plantArray, patterns, minPlant - 2)
-              assignNewPlant(nextGen, plantArray, patterns, maxPlant + 1)
-              assignNewPlant(nextGen, plantArray, patterns, maxPlant + 2)
-       
-              for j in plantArray:
-                     assignNewPlant(nextGen, plantArray, patterns, j)
-                     
-              plantArray = nextGen
-              minPlant = minPlant - 2
-              maxPlant = maxPlant + 2
-              
-       totalPlants = sum(key for key,value in plantArray.items() if value =="#")
-              
-       return totalPlants
+		oldTotal = totalPlants
+		totalPlants = sumTotalPlants(plantArray)
+		
+		totalDiff = totalPlants - oldTotal
+		print(totalDiff)
+		if totalDiff == lastTotalDiff:
+			repeatingGen = i
+			totalAtRepeat = totalPlants
+			break
+		lastTotalDiff = totalDiff
+	
+	return totalAtRepeat + totalDiff * (50000000000 - repeatingGen)
             
 input = Common.inputAsLines()
 #input = Common.inputAsString()
