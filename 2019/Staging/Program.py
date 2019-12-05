e2b6b2497c6ae856a4e3cf360ce1a963ee77a49b
Quinn Ramsay <quinnramsay@gmail.com>
2019-12-05 14:55:17 -0700

    day 5
    Needs some cleanup but completed

--- 2019/Staging/Program.py
+++ 2019/Staging/Program.py
@@ -1,5 +1,6 @@
 import Common
 
+
 def get_op_code(augmented_op):
     return augmented_op % 100
 
@@ -39,6 +40,42 @@ def mult(registers, ip):
     return ip + size
 
 
+def jmp_true(registers, ip):
+    size = 3
+    augmented_op, first, second = get_inputs(registers, ip, size)
+    param_modes = [get_param_mode(augmented_op, num) for num in range(size - 1)]
+    if get_value(registers, first, param_modes[0]) != 0:
+        return get_value(registers, second, param_modes[1])
+    else:
+        return ip + size
+
+
+def jmp_false(registers, ip):
+    size = 3
+    augmented_op, first, second = get_inputs(registers, ip, size)
+    param_modes = [get_param_mode(augmented_op, num) for num in range(size - 1)]
+    if get_value(registers, first, param_modes[0]) == 0:
+        return get_value(registers, second, param_modes[1])
+    else:
+        return ip + size
+
+
+def less_than(registers, ip):
+    size = 4
+    augmented_op, noun, verb, result = get_inputs(registers, ip, size)
+    param_modes = [get_param_mode(augmented_op, num) for num in range(size - 1)]
+    registers[result] = get_value(registers, noun, param_modes[0]) < get_value(registers, verb, param_modes[1])
+    return ip + size
+
+
+def equals(registers, ip):
+    size = 4
+    augmented_op, noun, verb, result = get_inputs(registers, ip, size)
+    param_modes = [get_param_mode(augmented_op, num) for num in range(size - 1)]
+    registers[result] = get_value(registers, noun, param_modes[0]) == get_value(registers, verb, param_modes[1])
+    return ip + size
+
+
 def inp(registers, ip):
     size = 2
     augmented_op, result = get_inputs(registers, ip, size)
@@ -46,40 +83,57 @@ def inp(registers, ip):
     registers[result] = in_val
     return ip + size
 
+
 def out(registers, ip):
     size = 2
-    augmented_op, out_val_pos = get_inputs(registers, ip, size)
-    print("Output: ", registers[out_val_pos])
+    augmented_op, out_val = get_inputs(registers, ip, size)
+    param_modes = [get_param_mode(augmented_op, num) for num in range(size - 1)]
+    print("Output: ", get_value(registers, out_val, param_modes[0]))
     return ip + size
 
-instructions = {
-    1: add,
-    2: mult,
-    3: inp,
-    4: out
-}
 
-def run_int_program(int_string, noun, verb):
-    numbers = Common.numbers(int_string)
+class IntProgram:
+    def __init__(self, instructions):
+        self.instructions = instructions
 
-    index = 0
-    while True:
-        op_code = get_op_code(numbers[index])
-        if op_code == 99:
-            break
-        elif op_code in instructions:
-            index = instructions[op_code](numbers, index)
+    def run(self, registers):
+        index = 0
+        while True:
+            op_code = get_op_code(registers[index])
+            if op_code == 99:
+                break
+            elif op_code in self.instructions:
+                instruction = self.instructions[op_code]
+                index = instruction(registers, index)
 
 
-def part1(int_string):
-    base_noun = 12
-    base_verb = 2
-    return run_int_program(int_string, base_noun, base_verb)
+def part1(registers):
+    instructions = {
+        1: add,
+        2: mult,
+        3: inp,
+        4: out
+    }
+    program = IntProgram(instructions)
+    return program.run(registers)
 
-print(get_op_code(1002) == 2)
-print(get_param_mode(1002, 0) == 0)
-print(get_param_mode(1002, 1) == 1)
+def part2(registers):
+    instructions = {
+        1: add,
+        2: mult,
+        3: inp,
+        4: out,
+        5: jmp_true,
+        6: jmp_false,
+        7: less_than,
+        8: equals
+    }
+    program = IntProgram(instructions)
+    return program.run(registers)
+
+
 input_string = Common.inputAsString()
+initial_registers = Common.numbers(input_string)
 
-print(part1(input_string))
-#print(part2(input_string) == 5398)
+part1(initial_registers[:])
+part2(initial_registers[:])
