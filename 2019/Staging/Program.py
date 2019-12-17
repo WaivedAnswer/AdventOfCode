8e58a8b9007079a31e61af962222bc298359cb7f
Quinn Ramsay <quinnramsay@gmail.com>
2019-12-16 17:40:26 -0700

    WIP

--- 2019/Staging/Program.py
+++ 2019/Staging/Program.py
@@ -8,27 +8,26 @@ def get_repeated(pattern, repeat):
     return repeated
 
 
-def transform(in_val, pattern = [0,1,0,-1]):
+def transform(in_val, pattern = (0,1,0,-1)):
     out_digs = []
-    str_val = "0" + str(in_val)
-    for repeat, dig in enumerate(str(in_val)):
+    str_val = "0" + in_val
+    for repeat, dig in enumerate(in_val):
         repeat += 1
         repeated_pattern = itertools.cycle(get_repeated(pattern, repeat))
         zipped = list(zip(str_val, repeated_pattern))
-        print(zipped)
-        to_sum = [ int(val) * pattern_val for val, pattern_val in zipped ]
-        print(to_sum)
+
+        to_sum = [int(val) * pattern_val for val, pattern_val in zipped]
+
         out_dig = abs(sum(to_sum)) % 10
 
-        out_digs.append(out_dig)
-    return out_digs
+        out_digs.append(str(out_dig))
+
+    return "".join(out_digs)
 
 
-input_signal = 12345678
+input_signal = Common.inputAsString()
 output_signal = None
-print(transform(12345678))
 for _ in range(100):
-    output_signal = transform(input_signal)
-    input_signal = output_signal
+    input_signal = transform(input_signal)
 
-orbit_graph = nx.Graph()"""
+print(input_signal[0:8])
